eac4f61fe4bb33be1505f97f8bd39e3a1340b7f9
Neni <felipe.dacs@gmail.com>
2023-08-17 21:09:52 -0300

    lint

--- src/fibonacci.lua
+++ src/fibonacci.lua
@@ -4,22 +4,22 @@ local function fibonacci(n)
 end
 
 local tdt = {
-  {0, 0},
-  {1, 1},
-  {2, 1},
-  {3, 2},
-  {4, 3},
-  {5, 5},
-  {6, 8},
-  {7, 13},
-  {8, 21},
-  {9, 34},
-  {10, 55},
+  { 0,  0 },
+  { 1,  1 },
+  { 2,  1 },
+  { 3,  2 },
+  { 4,  3 },
+  { 5,  5 },
+  { 6,  8 },
+  { 7,  13 },
+  { 8,  21 },
+  { 9,  34 },
+  { 10, 55 },
 }
 
 for _, scenario in ipairs(tdt) do
   local input = scenario[1]
   local want = scenario[2]
   local got = fibonacci(input)
-  assert(got == want, "with "..input..": "..want.." == "..got);
+  assert(got == want, "with " .. input .. ": " .. want .. " == " .. got);
 end
